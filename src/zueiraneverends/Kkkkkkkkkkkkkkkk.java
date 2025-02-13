395f011d87ed75fd554fd5e24b44fdb49db072ea
Gabriel <gfelixfaustina@gmail.com>
2025-02-13 20:11:56 -0300

    Kronicamente Guilhermístico

---
+++ src/zueiraneverends/Kkkkkkkkkkkkkkkk.java
@@ -0,0 +1,37 @@
+package zueiraneverends;
+
+import java.time.ZoneId;
+
+public class Kkkkkkkkkkkkkkkk {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		double numeroSoQueQuebrado;
+		
+		boolean bundeano;
+		
+		int[] vector;
+		
+		//////////////////////////////////////////////////////////////
+		int[][] matrix = {
+				{1, 3, 3},
+				{3, 1, 1}
+		};
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.println(matrix[i][j]);
+			}
+		}
+		//////////////////////////////////////////////////////////////
+		
+		char xa;
+		String texto;
+		
+//		for (String zones: ZoneId.getAvailableZoneIds()) {
+//			System.out.println(zones);
+//		}
+	}
+}
